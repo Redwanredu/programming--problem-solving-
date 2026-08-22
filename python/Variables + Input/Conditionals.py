d415799b560234e0ee6817796a527199a4806d5d
Redwanredu <158178872+Redwanredu@users.noreply.github.com>
2026-08-22 14:25:49 +0200

    p

---
+++ python/Variables + Input/Conditionals.py
@@ -0,0 +1,10 @@
+number = int(input("write a number "))
+if number>0 and number%2==0:
+    print("positive  and even")
+elif number>0 and number%2==1 :
+    print("positive  and Odd")
+elif number<0 :
+    print("negative")
+else:
+    print("zero")
+    
---
+++ python/Variables + Input/Output.py
@@ -0,0 +1,3 @@
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+print(f"Hello {name}, next year you'll be {age+1} years old!")
