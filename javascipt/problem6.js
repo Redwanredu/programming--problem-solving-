bd8d84a2f6313bd3757a85875f6db76bcb1ccba9
Redwanredu <158178872+Redwanredu@users.noreply.github.com>
2026-09-07 09:49:48 +0200

    Create problem6.js

---
+++ javascipt/problem6.js
@@ -0,0 +1,11 @@
+let str1 = "listen";
+let str2 = "silent";
+
+let sorted1 = str1.split("").sort().join("");
+let sorted2 = str2.split("").sort().join("");
+
+if (sorted1 === sorted2) {
+    console.log("They are anagrams");
+} else {
+    console.log("They are not anagrams");
+}
